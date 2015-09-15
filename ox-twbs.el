b54058ae4e26e8ce2318c36e5b7369cbbed7c4b1
marsmining <marsmining@gmail.com>
2015-09-15 10:37:53 +0200

    inc version to 1.0.3

--- ox-twbs.el
+++ ox-twbs.el
@@ -8,7 +8,7 @@
 ;;         Brandon van Beekum <marsmining at gmail dot com>
 ;; URL: https://github.com/marsmining/ox-twbs
 ;; Keywords: org, html, publish, twitter, bootstrap
-;; Version: 1.0.2
+;; Version: 1.0.3
 
 ;; This file is not part of GNU Emacs.
 
